3df921036b03624b192e1ea8d1a8e2f9793d6cd2
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-04 16:21:32 +0100

    Refactor: private methods refactored to body functions

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -2,8 +2,8 @@ namespace Tennis
 {
     public class TennisGame1 : ITennisGame
     {
-        private int _player1Score = 0;
-        private int _player2Score = 0;
+        private int _player1Score;
+        private int _player2Score;
         private readonly string _player1Name;
         private readonly string _player2Name;
 
@@ -20,12 +20,7 @@ namespace Tennis
             else
                 _player2Score += 1;
         }
-
-        private bool IsPlayer1(string playerName)
-        {
-            return playerName == _player1Name;
-        }
-
+        
         public string GetScore()
         {
             return BothPlayersAreDraw() ? GetLoveScore() :
@@ -33,46 +28,35 @@ namespace Tennis
                 GetCurrentScore();
         }
 
-        private bool BothPlayersAreDraw()
-        {
-            return _player1Score == _player2Score;
-        }
+        private bool IsPlayer1(string playerName) => playerName == _player1Name;
 
-        private string GetLoveScore()
-        {
-            return _player1Score switch
+
+        private bool BothPlayersAreDraw() => _player1Score == _player2Score;
+
+        private string GetLoveScore() =>
+            _player1Score switch
             {
                 0 => "Love-All",
                 1 => "Fifteen-All",
                 2 => "Thirty-All",
                 _ => "Deuce"
             };
-        }
 
-        private bool MatchIsInAdvantagePhase()
-        {
-            return _player1Score >= 4 || _player2Score >= 4;
-        }
+        private bool MatchIsInAdvantagePhase() => _player1Score >= 4 || _player2Score >= 4;
 
-        private string GetAdvantageScore()
-        {
-            return (_player1Score - _player2Score) switch
+        private string GetAdvantageScore() =>
+            (_player1Score - _player2Score) switch
             {
                 1 => $"Advantage {_player1Name}",
                 -1 => $"Advantage {_player2Name}",
                 >= 2 => $"Win for {_player1Name}",
                 _ => $"Win for {_player2Name}"
             };
-        }
 
-        private string GetCurrentScore()
-        {
-            return GetFormattedScoreFrom(_player1Score) +"-"+GetFormattedScoreFrom(_player2Score);
-        }
+        private string GetCurrentScore() => GetFormattedScoreFrom(_player1Score) +"-"+GetFormattedScoreFrom(_player2Score);
 
-        private static string GetFormattedScoreFrom(int playerScore)
-        {
-            return playerScore switch
+        private static string GetFormattedScoreFrom(int playerScore) =>
+            playerScore switch
             {
                 0 => "Love",
                 1 => "Fifteen",
@@ -80,7 +64,6 @@ namespace Tennis
                 3 => "Forty",
                 _ => ""
             };
-        }
     }
 }
 
