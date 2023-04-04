07c4de5db1b9fa899dd20f380809f7bc950a616e
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-04 13:21:56 +0100

    Refactor: SetScore method extracted

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -70,29 +70,36 @@ namespace Tennis
         {
             for (var i = 1; i < 3; i++)
             {
-                var tempScore = 0;
-                if (i == 1) tempScore = m_score1;
-                else
-                {
-                    score += "-";
-                    tempScore = m_score2;
-                }
+                score = SetScore(score, i);
+            }
 
-                switch (tempScore)
-                {
-                    case 0:
-                        score += "Love";
-                        break;
-                    case 1:
-                        score += "Fifteen";
-                        break;
-                    case 2:
-                        score += "Thirty";
-                        break;
-                    case 3:
-                        score += "Forty";
-                        break;
-                }
+            return score;
+        }
+
+        private string SetScore(string score, int i)
+        {
+            var tempScore = 0;
+            if (i == 1) tempScore = m_score1;
+            else
+            {
+                score += "-";
+                tempScore = m_score2;
+            }
+
+            switch (tempScore)
+            {
+                case 0:
+                    score += "Love";
+                    break;
+                case 1:
+                    score += "Fifteen";
+                    break;
+                case 2:
+                    score += "Thirty";
+                    break;
+                case 3:
+                    score += "Forty";
+                    break;
             }
 
             return score;
