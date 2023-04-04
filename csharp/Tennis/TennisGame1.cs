362423f1af2de0332913ad5887c77dd900034399
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-04 13:16:43 +0100

    Refactor: normal score method extracted

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -9,19 +9,19 @@ namespace Tennis
 
         public TennisGame1(string player1Name, string player2Name)
         {
-            this._player1Name = player1Name;
-            this._player2Name = player2Name;
+            _player1Name = player1Name;
+            _player2Name = player2Name;
         }
 
         public void WonPoint(string playerName)
         {
-            if (Player1Scored(playerName))
+            if (IsPlayer1(playerName))
                 m_score1 += 1;
             else
                 m_score2 += 1;
         }
 
-        private bool Player1Scored(string playerName)
+        private bool IsPlayer1(string playerName)
         {
             return playerName == _player1Name;
         }
@@ -29,39 +29,9 @@ namespace Tennis
         public string GetScore()
         {
             var score = "";
-            if (BothPlayersAreDraw())
-            {
-                score = SetDraw();
-            }
-            else if (MatchIsInAdvantagePhase())
-            {
-                score = SetAdvantageScore();
-            }
-            else
-            {
-                for (var i = 1; i < 3; i++)
-                {
-                    var tempScore = 0;
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
-                }
-            }
-            return score;
+            if (BothPlayersAreDraw()) return SetLoveScore();
+            if (MatchIsInAdvantagePhase()) return SetAdvantageScore();
+            return SetNormalScore(score);
         }
 
         private bool BothPlayersAreDraw()
@@ -69,7 +39,7 @@ namespace Tennis
             return m_score1 == m_score2;
         }
 
-        private string SetDraw()
+        private string SetLoveScore()
         {
             string score;
             score = m_score1 switch
@@ -98,6 +68,38 @@ namespace Tennis
                 >= 2 => "Win for player1",
                 _ => "Win for player2"
             };
+            return score;
+        }
+
+        private string SetNormalScore(string score)
+        {
+            for (var i = 1; i < 3; i++)
+            {
+                var tempScore = 0;
+                if (i == 1) tempScore = m_score1;
+                else
+                {
+                    score += "-";
+                    tempScore = m_score2;
+                }
+
+                switch (tempScore)
+                {
+                    case 0:
+                        score += "Love";
+                        break;
+                    case 1:
+                        score += "Fifteen";
+                        break;
+                    case 2:
+                        score += "Thirty";
+                        break;
+                    case 3:
+                        score += "Forty";
+                        break;
+                }
+            }
+
             return score;
         }
     }
