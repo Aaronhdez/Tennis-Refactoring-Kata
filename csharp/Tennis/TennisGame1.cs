948ace55ef2c33ea968b6d601c9fbae422ea41a4
Aarón Hernández Álvarez <aahernandez@domingoalonsogroup.com>
2023-04-04 13:12:32 +0100

    Refactor: Advantage methods extracted to improve legibility

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -4,43 +4,44 @@ namespace Tennis
     {
         private int m_score1 = 0;
         private int m_score2 = 0;
-        private string player1Name;
-        private string player2Name;
+        private readonly string _player1Name;
+        private string _player2Name;
 
         public TennisGame1(string player1Name, string player2Name)
         {
-            this.player1Name = player1Name;
-            this.player2Name = player2Name;
+            this._player1Name = player1Name;
+            this._player2Name = player2Name;
         }
 
         public void WonPoint(string playerName)
         {
-            if (playerName == "player1")
+            if (Player1Scored(playerName))
                 m_score1 += 1;
             else
                 m_score2 += 1;
         }
 
+        private bool Player1Scored(string playerName)
+        {
+            return playerName == _player1Name;
+        }
+
         public string GetScore()
         {
-            string score = "";
-            var tempScore = 0;
-            if (IsADraw())
+            var score = "";
+            if (BothPlayersAreDraw())
             {
                 score = SetDraw();
             }
-            else if (m_score1 >= 4 || m_score2 >= 4)
+            else if (MatchIsInAdvantagePhase())
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                score = SetAdvantageScore();
             }
             else
             {
                 for (var i = 1; i < 3; i++)
                 {
+                    var tempScore = 0;
                     if (i == 1) tempScore = m_score1;
                     else { score += "-"; tempScore = m_score2; }
                     switch (tempScore)
@@ -63,7 +64,7 @@ namespace Tennis
             return score;
         }
 
-        private bool IsADraw()
+        private bool BothPlayersAreDraw()
         {
             return m_score1 == m_score2;
         }
@@ -77,6 +78,25 @@ namespace Tennis
                 1 => "Fifteen-All",
                 2 => "Thirty-All",
                 _ => "Deuce"
+            };
+            return score;
+        }
+
+        private bool MatchIsInAdvantagePhase()
+        {
+            return m_score1 >= 4 || m_score2 >= 4;
+        }
+
+        private string SetAdvantageScore()
+        {
+            string score;
+            var minusResult = m_score1 - m_score2;
+            score = minusResult switch
+            {
+                1 => "Advantage player1",
+                -1 => "Advantage player2",
+                >= 2 => "Win for player1",
+                _ => "Win for player2"
             };
             return score;
         }
